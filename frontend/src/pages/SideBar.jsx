6729fab54f739152ab0c55224cfa962af33f93be
XKenda <elmlonxxr@gmail.com>
2026-04-12 23:06:37 +0200

    implement basic idea of sidebar

--- frontend/src/pages/SideBar.jsx
+++ frontend/src/pages/SideBar.jsx
@@ -1,9 +1,27 @@
-
+import { TextAlignJustify, X } from "lucide-react"
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const SideBar = () => {
-    return (
-        <div className="sidebar-con">
+    const [isOpen, setIsOpen] = useState(false)
 
+
+    return (
+        <div className="sidebar-con fixed h-dvh w-full sm:w-40">
+            <div className="header flex justify-between pt-2">
+                <p className="tile ml-2">Side bar</p>
+                <TextAlignJustify className="btn" size={30}  />
+            </div>
+            <hr className="my-3" />
+            <div className="pages-list-con pl-2">
+                <ul className="pages-list">
+                    <li><NavLink className={({ isActive }) =>
+                        isActive ? "text-red-500" : "text-black"
+                        } to={'/home'} >
+                        Home
+                    </NavLink></li>
+                </ul>
+            </div>
         </div>
     )
 }
