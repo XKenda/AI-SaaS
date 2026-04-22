5dcd5ba872bb088c83cff4bc908d266f4c3671d4
XKenda <elmlonxxr@gmail.com>
2026-04-22 11:22:19 +0200

    changed some details

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import { PORT } from "./src/config/env.js";
 import app from "./src/app.js";
 
+const port = PORT || 3000
 
 
-app.listen(PORT, ()=>{
-    console.log("app listened");
+app.listen(port, ()=>{
+    console.log(`app listened on port ${port}`);
 })
