b24bc18aaada0cec704446db7316fd88bceea34c
XKenda <elmlonxxr@gmail.com>
2026-08-15 17:49:27 +0300

    fix upload error

--- backend/src/utils/uploadImageToCloudinary.js
+++ backend/src/utils/uploadImageToCloudinary.js
@@ -1,11 +1,26 @@
-import { v2 as cloudinary } from "cloudinary"
+import { v2 as cloudinary } from "cloudinary";
 
 export const uploadImageToCloudinary = async (file) => {
     try {
-        const result = await cloudinary.uploader.upload(file.path)
+        const result = await new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream(
+                {
+                    resource_type: "image"
+                },
+                (error, result) => {
+                    if (error) {
+                        reject(error);
+                    } else {
+                        resolve(result);
+                    }
+                }
+            );
 
-        return result
+            stream.end(file.buffer);
+        });
+
+        return result;
     } catch (e) {
-        throw new Error(e.message)
+        throw new Error(e.message);
     }
-}
+};
